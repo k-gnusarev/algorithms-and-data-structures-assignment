ecec75d62fcc2bab87cf4fdbe5bab3852673400d
Konstantin Gnusarev <kgnusarev@griddynamics.com>
2023-04-08 12:48:46 -0600

    reverse linked list

---
+++ reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,14 @@
+function reverseList(head) {
+  let prev = null
+  let current = head
+
+  while (current) {
+    const next = current.next
+
+    current.next = prev
+    prev = current
+    current = next
+  }
+
+  return prev
+}
